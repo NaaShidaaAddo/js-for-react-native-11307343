89fe9dfeb9083e07b0a0a88d1d9df418d4797063
Naa Shidaa <rainaaaddo@gmail.com>
2024-05-19 16:12:41 +0000

    Task 3

---
+++ userInfo.js
@@ -0,0 +1,20 @@
+const { processArray, formatArrayStrings } = require('./arrayManipulation');
+
+
+//formattedArrayStrings is the strings array formatted by formatArrayStrings
+function createUserProfiles(names, formattedArrayStrings) {
+    return names.map((name, index) => {
+        return {
+            originalName: name,
+            modifiedName: formattedArrayStrings[index],
+            id: index + 1
+        };
+    });
+}
+
+
+const stringArray = ["Ama", "Sena", "Whitfield", "Seana", "Dokdo"];
+const formattedArrayStrings = ["ama", "SENA", "whitfield", "SEANA", "dokdo"];
+
+const userProfiles = createUserProfiles(stringArray, formattedArrayStrings);
+console.log(userProfiles); 
